4b87d534c2c75bed0812207e5fef7ed06b3991c7
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-06-08 21:26:59 +0900

    feat(Swagger): auth 서버 swagger 적용

--- src/main/java/com/nhnacademy/auth/controller/AuthController.java
+++ src/main/java/com/nhnacademy/auth/controller/AuthController.java
@@ -14,9 +14,16 @@ import com.nhnacademy.auth.model.dto.request.RequestJwtTokenDTO;
 import com.nhnacademy.auth.model.dto.response.ResponseJwtTokenDTO;
 import com.nhnacademy.auth.service.JwtService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "JWT 생성 및 발급", description = "JWT 생성 및 발급 기능 제공")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/token")
@@ -27,9 +34,15 @@ public class AuthController {
 	/**
 	 * front에서 JWT Token create 요청이 오면 요청을 처리하는 RestController
 	 */
+	@Operation(summary = "JWT 생성 및 발급", description = "JWT 생성 및 발급 요청 처리",
+		responses = {
+			@ApiResponse(responseCode = "201", description = "JWT 생성 및 발급 요청에 따른 성공 응답"),
+			@ApiResponse(responseCode = "400", description = "입력값 검증 실패", content = @Content(schema = @Schema(implementation = ValidationFailedException.class)))
+		})
 	@PostMapping
-	public ResponseEntity<ResponseJwtTokenDTO> createJwtToken(
-		@Validated @RequestBody RequestJwtTokenDTO requestJwtTokenDTO,
+	public ResponseEntity<ResponseJwtTokenDTO> createJwtToken(@Validated
+		@Parameter(description = "JWT 생성 및 발급 DTO", required = true, schema = @Schema(implementation = RequestJwtTokenDTO.class))
+		@RequestBody RequestJwtTokenDTO requestJwtTokenDTO,
 		BindingResult bindingResult,
 		HttpServletResponse response) {
 		if (bindingResult.hasErrors()) {
